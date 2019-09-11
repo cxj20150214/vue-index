0613ae179481af6668883d15b0969b1131cb9a36
cxj20150214 <848935682@qq.com>
2019-09-11 11:01:50 +0800

    111

--- src/views/address/address.vue
+++ src/views/address/address.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
        <div class="addressBg">
+          <div class="address_box">
+            <div class="city">
 
+            </div>
+            <div class="price"></div>
+          </div>
        </div>
     </div>
 </template>
@@ -22,8 +27,22 @@ export default {
 <style lang="less" scoped>
 .addressBg{
   width: 750px;
+  height: auto;
+  display: flex;
   margin: 0px auto;
-  background-image: url('../../img/address-bg.jpg')
+  background-image: url('../../img/address-bg.jpg');
+  background-size: 100% 18vh;
+  background-repeat: no-repeat;
+  .address_box{
+    width:90%;
+    margin:50px auto;
+    .city{
+      background-color: #fff;
+      border-radius:10px;
+      height: 195px;
+      box-shadow: 0px 5px 10px rgba(0,0,0,.1);
+    }
+  }
 }
   @media screen and (max-width: 750px) {
     .addressBg{
